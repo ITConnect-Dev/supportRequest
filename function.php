099ddf22b56b4ecf5906c66e273e7d555fb66fd0
ITConnect-Dev <58284806+ITConnect-Dev@users.noreply.github.com>
2020-01-06 10:57:07 -0800

    Add files via upload

--- function.php
+++ function.php
@@ -1,16 +1,16 @@
 <?php
-if(isset($_POST['submit'])){
-    $to = "jakemckenzie19@gmail.com"; // this is your Email address
-    $from = "jakemckenzie19@gmail.com"; // this is your Email address
-    $auth_name = $_POST['#authName'];
-    $req_name = $_POST['#reqName'];
-    $user_name = $_POST['#username'];
-    $position_name = $_POST['#position'];
+if(isset($_POST['requestForm'])){
+    $to = "jmckenzie@it-connect.com"; // this is your Email address
+    $from = "jmckenzie@it-connect.com"; // this is your Email address
+    $auth_name = $_POST['authName'];
+    $req_name = $_POST['reqName'];
+    $user_name = $_POST['username'];
+    $position_name = $_POST['position'];
     $subject = "Form submission";
-    $message = $auth_name . " authorized this request by:" . $req_name . " for:" . "\n\n" . $_POST[" "];
+    $message = $auth_name . " authorized this request by:" . $req_name . " for:" . "\n\n" . $_POST[$user_name . " " . $position_name . " "];
 
     mail($to,$subject,$message, "From: <$from>");
-    //echo "Mail Sent. Thank you " . $req_name . ", we will contact you shortly.";
+    echo "Mail Sent. Thank you " . $req_name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     }
 ?>
